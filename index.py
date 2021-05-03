7fdebff847ea3081594e61ddbd5abaad2e8e1dcc
the-timoye <thetimoye@gmail.com>
2021-05-03 14:14:18 +0100

    feat(Question 2): Count female users

--- index.py
+++ index.py
@@ -8,9 +8,19 @@ spark_df = spark_session.read.json('data/sparkify_log_small.json')
 spark_df.printSchema()
 
 
-# QUESTION 1: Which page did user id "" not visit?
+print('==================== QUESTION 1: Which page did user id "" not visit? ====================')
 all_pages = spark_df.select('page').dropDuplicates()
 get_user_pages = spark_df.select('page').where(spark_df['userId'] == "").dropDuplicates()
 non_visited_pages = set(all_pages.collect()) - set(get_user_pages.collect())
 for row in non_visited_pages:
     print(row.page)
+
+
+print('==================== Question 2: How many females are in the dataset? ====================')
+count_females = spark_df.select('userId').where(spark_df.gender == 'F').dropDuplicates().count()
+print(count_females)
+
+print('==================== Question 3: How many songs were played form the most played artist? ====================')
+
+
+print('==================== Question 4: How many songs do users listen to on average between visiting our home page? ====================')
